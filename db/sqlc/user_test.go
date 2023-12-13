7bf693ede038f9126c7acfc95e90558a07fd39fe
PfMartin <martinpfatrisch@gmail.com>
2023-12-13 16:53:42 +0100

    Added first test the user table

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -1,6 +1,35 @@
 package db
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/PfMartin/wegonice-api/util"
+	"github.com/stretchr/testify/require"
+)
 
 func createRandomUser(t *testing.T) User {
+	hashedPassword, err := util.HashPassword(util.RandomString(6))
+	require.NoError(t, err)
+
+	arg := CreateUserParams{
+		Email:          util.RandomEmail(),
+		HashedPassword: hashedPassword,
+	}
+
+	user, err := testQueries.CreateUser(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, arg.Email, user.Email)
+	require.Equal(t, arg.HashedPassword, user.HashedPassword)
+
+	require.True(t, user.PasswordChangedAt.IsZero())
+	require.NotZero(t, user.CreatedAt)
+
+	return user
+}
+
+func TestCreateUser(t *testing.T) {
+	createRandomUser(t)
 }
