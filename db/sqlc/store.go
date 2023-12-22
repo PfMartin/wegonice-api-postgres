04bf95ffb3cfaa780eda8c68eababb891b3a8486
PfMartin <martinpfatrisch@gmail.com>
2023-12-22 10:59:19 +0100

    Added CreateRecipe Transaction and transaction decorator

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -1,7 +1,9 @@
 package db
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 )
 
 // Store provides all functions to execute db queries and transactions
@@ -20,4 +22,65 @@ func NewStore(db *sql.DB) Store {
 		db:      db,
 		Queries: New(db),
 	}
+}
+
+// Executes a function within a database transaction
+// Rolls back all changes if any action within the transaction fails
+func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("transaction error: %v, rollback error: %v", err, rbErr)
+		}
+	}
+
+	return tx.Commit()
+}
+
+type CreateRecipeTxResult struct {
+	Recipe            Recipe             `json:"recipe"`
+	RecipeIngredients []RecipeIngredient `json:"recipe_ingredients"`
+	RecipeSteps       []RecipeStep       `json:"recipe_steps"`
+}
+
+func (store *SQLStore) CreateRecipeTx(ctx context.Context, recipeArg CreateRecipeParams, ingredientsArg []CreateRecipeIngredientParams, stepsArg []CreateRecipeStepParams) (CreateRecipeTxResult, error) {
+	var result CreateRecipeTxResult
+
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+
+		result.Recipe, err = q.CreateRecipe(context.Background(), recipeArg)
+		if err != nil {
+			return err
+		}
+
+		for _, ingredient := range ingredientsArg {
+			recipeIngredient, err := q.CreateRecipeIngredient(context.Background(), ingredient)
+			if err != nil {
+				return err
+			}
+
+			result.RecipeIngredients = append(result.RecipeIngredients, recipeIngredient)
+		}
+
+		for _, step := range stepsArg {
+			recipeStep, err := q.CreateRecipeStep(context.Background(), step)
+			if err != nil {
+				return err
+			}
+
+			result.RecipeSteps = append(result.RecipeSteps, recipeStep)
+		}
+
+		return nil
+	})
+
+	return result, err
+
 }
